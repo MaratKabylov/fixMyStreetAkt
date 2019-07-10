61cd3a1a677d5f080b0bf66e5986d9ad835f5a4b
Struan Donald <struan@exo.org.uk>
2019-07-10 16:02:03 +0100

    [IsleOfWight] example labels on litter bin assets

--- web/cobrands/isleofwight/assets.js
+++ web/cobrands/isleofwight/assets.js
@@ -21,18 +21,60 @@ var defaults = {
         'fixmystreet': 1.194328566789627
     },
     min_resolution: 0.00001,
-    asset_id_field: 'CentralAssetId',
-    attributes: {
-        central_asset_id: 'CentralAssetId',
-        asset_details: 'FeatureId'
-    },
+    asset_id_field: 'asset_id',
     geometryName: 'msGeometry',
     srsName: "EPSG:27700",
     strategy_class: OpenLayers.Strategy.FixMyStreet,
     body: "Isle of Wight Council"
 };
 
+var bin_code_to_type = {
+    'DOG': 'dog bin',
+    'LITT': 'litter bin'
+};
+
+var pin_prefix = fixmystreet.pin_prefix || document.getElementById('js-map-data').getAttribute('data-pin_prefix');
+
+var labeled_default = {
+    fillColor: "#FFFF00",
+    fillOpacity: 0.6,
+    strokeColor: "#000000",
+    strokeOpacity: 0.8,
+    strokeWidth: 2,
+    pointRadius: 6
+};
+
+var labeled_select = {
+    externalGraphic: pin_prefix + "pin-spot.png",
+    fillColor: "#55BB00",
+    graphicWidth: 48,
+    graphicHeight: 64,
+    graphicXOffset: -24,
+    graphicYOffset: -56,
+    backgroundGraphic: pin_prefix + "pin-shadow.png",
+    backgroundWidth: 60,
+    backgroundHeight: 30,
+    backgroundXOffset: -7,
+    backgroundYOffset: -22,
+    popupYOffset: -40,
+    graphicOpacity: 1.0,
+
+    label: "${asset_id}",
+    labelOutlineColor: "white",
+    labelOutlineWidth: 3,
+    labelYOffset: 65,
+    fontSize: '15px',
+    fontWeight: 'bold'
+};
+
+var labeled_stylemap = new OpenLayers.StyleMap({
+  'default': new OpenLayers.Style(labeled_default),
+  'select': new OpenLayers.Style(labeled_select)
+});
+
 fixmystreet.assets.add($.extend(true, {}, defaults, {
+    select_action: true,
+    stylemap: labeled_stylemap,
     asset_category: [
         "Empty Bin",
         "Litter Bin Overflowing"
@@ -46,13 +88,24 @@ fixmystreet.assets.add($.extend(true, {}, defaults, {
         'isleofwight': 6.614596562526458,
         'fixmystreet': 4.777314267158508
     },
-    usrn: {
-        attribute: 'SITE_CODE',
-        field: 'site_code'
-    },
     asset_item: "bin",
     filter_key: 'feature_type_code',
-    filter_value: 'LITT'
+    filter_value: 'LITT',
+    actions: {
+        asset_found: function(asset) {
+          var id = asset.attributes.asset_id || '';
+          if (id !== '') {
+              var code = asset.attributes.feature_type_code;
+              var asset_name = bin_code_to_type[code] || this.fixmystreet.asset_item;
+              $('.category_meta_message').html('You have selected ' + asset_name + ' <b>' + id + '</b>');
+          } else {
+              $('.category_meta_message').html('You can pick a <b class="asset-spot">' + this.fixmystreet.asset_item + '</b> from the map &raquo;');
+          }
+        },
+        asset_not_found: function() {
+           $('.category_meta_message').html('You can pick a <b class="asset-spot">' + this.fixmystreet.asset_item + '</b> from the map &raquo;');
+        }
+    }
 }));
 
 fixmystreet.assets.add($.extend(true, {}, defaults, {
@@ -67,10 +120,6 @@ fixmystreet.assets.add($.extend(true, {}, defaults, {
     max_resolution: {
         'isleofwight': 6.614596562526458,
         'fixmystreet': 4.777314267158508
-    },
-    usrn: {
-        attribute: 'SITE_CODE',
-        field: 'site_code'
     },
     asset_item: "bin",
     filter_key: 'feature_type_code',
